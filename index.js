8aefdeb682c5529990b94f41255589140c8b7e22
osistlk <osistlk@gmail.com>
2024-02-23 19:39:21 -0500

    Add .mp4 files to .gitignore and update video URL

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const { downloadAndMergeVideo } = require("./download_yt");
 
 const videos = [
-    'https://www.youtube.com/watch?v=OJZIjmzOOFE&list=PLRWvNQVqAeWJarmdncJk0jFhijoqi3PDo&index=6&pp=iAQB',
-    'https://www.youtube.com/watch?v=9VOmk-Q_1vM&list=PLRWvNQVqAeWJarmdncJk0jFhijoqi3PDo&index=7&pp=iAQB',
-    'https://www.youtube.com/watch?v=LpaAi4QHtqM&list=PLRWvNQVqAeWJarmdncJk0jFhijoqi3PDo&index=8&pp=iAQB',
-    'https://www.youtube.com/watch?v=Bgi01v9CVJc&list=PLRWvNQVqAeWJarmdncJk0jFhijoqi3PDo&index=9&pp=iAQB',
-    'https://www.youtube.com/watch?v=m_p2ro6uOcI&list=PLRWvNQVqAeWJarmdncJk0jFhijoqi3PDo&index=10&pp=iAQB'
+    'https://www.youtube.com/watch?v=sdOQ-jX0ymk'
 ];
 
 videos.forEach(videoUrl => {
