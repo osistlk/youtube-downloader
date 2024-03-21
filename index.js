acfbfe2d1ab9991a0743e7dd019b98271a9b346b
osistlk <osistlk@gmail.com>
2024-03-20 20:36:48 -0400

    Refactor video downloading and processing***

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-const { downloadAndMergeVideo } = require("./download_yt");
+const { downloadVideo, downloadAudio, processWithFFmpeg } = require("./download_yt");
 
-const videos = [
-    'https://www.youtube.com/watch?v=vNTxnYm81n8&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=1&pp=iAQB',
-    'https://www.youtube.com/watch?v=AJqiXQbB7rY&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=2&pp=iAQB',
-    'https://www.youtube.com/watch?v=9NsghRtIZAw&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=3&pp=iAQB',
-    'https://www.youtube.com/watch?v=C54-nSAihho&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=4&pp=iAQB',
-    'https://www.youtube.com/watch?v=nf8gzXKx1RA&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=5&pp=iAQB',
-    'https://www.youtube.com/watch?v=jRIkN_bywoM&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=6&pp=iAQB',
-    'https://www.youtube.com/watch?v=_gNZR5IEsAA&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=7&pp=iAQB',
-    'https://www.youtube.com/watch?v=cjFE4h2MUQI&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=8&pp=iAQB',
-    'https://www.youtube.com/watch?v=Rz6tvy_KP4I&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=9&pp=iAQB',
-    'https://www.youtube.com/watch?v=ppozUaQN-tM&list=PLRWvNQVqAeWI4Fpgkl3gMg2Rnx0dZ_s0r&index=10&pp=iAQB'
+const videoUrls = [
+    'https://www.youtube.com/watch?v=nm28m4gEOkI',
+    'https://www.youtube.com/watch?v=kx8yLdUJbqs',
+    'https://www.youtube.com/watch?v=xwU_MpVXpM4'
 ];
 
-videos.forEach(videoUrl => {
-    console.log('Download starting...');
-    const download = downloadAndMergeVideo(videoUrl);
-    download.then(() => console.log('Download complete!'));
-});
+async function downloadAndProcessVideos(ytVideoUrls) {
+    let videoPromises = [];
+    let audioPromises = [];
+
+    for (const videoUrl of ytVideoUrls) {
+        console.log('Download starting...');
+        videoPromises.push(downloadVideo(videoUrl));
+        audioPromises.push(downloadAudio(videoUrl));
+    }
+
+    const videos = await Promise.all(videoPromises);
+    const audios = await Promise.all(audioPromises);
+    console.log('Download completed.');
+
+    // if videos and audios are not equal in length, something went wrong
+    if (ytVideoUrls.length !== audios.length) {
+        throw new Error('Mismatch in video and audio downloads.');
+    }
+
+    console.log('Processing with FFmpeg...');
+
+    const processedVideos = [];
+    for (let i = 0; i < ytVideoUrls.length; i++) {
+        const videoPath = videos[i];
+        const audioPath = audios[i];
+        const processedVideo = await processWithFFmpeg(videoPath, audioPath);
+        processedVideos.push(processedVideo);
+    }
+
+    console.log('Processing completed.');
+    console.log('All done.');
+}
+
+downloadAndProcessVideos(videoUrls).catch(console.error);
