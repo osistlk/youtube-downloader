d9c7a3a176582137fa1fe19c1ab90d2c12c0f9ec
osistlk <osistlk@gmail.com>
2024-03-20 21:07:02 -0400

    Add support for fetching video URLs from a YouTube playlist

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { downloadVideo, downloadAudio, processWithFFmpeg } = require("./download_yt");
+const youtubePlaylist = require('youtube-playlist');
 
-const videoUrls = [
-    'https://www.youtube.com/watch?v=nm28m4gEOkI',
-    'https://www.youtube.com/watch?v=kx8yLdUJbqs',
-    'https://www.youtube.com/watch?v=xwU_MpVXpM4'
-];
+async function getVideoUrlsFromPlaylist(playlistUrl) {
+    const result = await youtubePlaylist(playlistUrl);
+    const videoUrls = result.data.playlist.map(video => video.url);
+    return videoUrls;
+}
 
 async function downloadAndProcessVideos(ytVideoUrls) {
     let videoPromises = [];
@@ -39,4 +40,19 @@ async function downloadAndProcessVideos(ytVideoUrls) {
     console.log('All done.');
 }
 
+let videoUrls = []
+
+const playlistUrl = 'https://www.youtube.com/playlist?list=PLRWvNQVqAeWIcz0Ky6pnrKkqsp3MA7o_n';
+videoUrls = getVideoUrlsFromPlaylist(playlistUrl)
+
+if (!playlistUrl && videoUrls.length === 0) {
+    // If not using a playlist, you can manually add video URLs
+    videoUrls = [
+        'https://www.youtube.com/watch?v=nm28m4gEOkI',
+        'https://www.youtube.com/watch?v=kx8yLdUJbqs',
+        'https://www.youtube.com/watch?v=xwU_MpVXpM4'
+    ];
+}
+
+console.log('Video URLs:', videoUrls);
 downloadAndProcessVideos(videoUrls).catch(console.error);
