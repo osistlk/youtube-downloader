5939ea39e3d104550579eacb09f6fdb8397adf19
osistlk <osistlk@gmail.com>
2024-03-20 21:14:01 -0400

    Refactor video downloading and processing

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
+const ytpl = require('ytpl');
 const { downloadVideo, downloadAudio, processWithFFmpeg } = require("./download_yt");
-const youtubePlaylist = require('youtube-playlist');
 
-async function getVideoUrlsFromPlaylist(playlistUrl) {
-    const result = await youtubePlaylist(playlistUrl);
-    const videoUrls = result.data.playlist.map(video => video.url);
-    return videoUrls;
-}
 
-async function downloadAndProcessVideos(ytVideoUrls) {
-    let videoPromises = [];
-    let audioPromises = [];
-
-    for (const videoUrl of ytVideoUrls) {
-        console.log('Download starting...');
-        videoPromises.push(downloadVideo(videoUrl));
-        audioPromises.push(downloadAudio(videoUrl));
-    }
-
-    const videos = await Promise.all(videoPromises);
-    const audios = await Promise.all(audioPromises);
-    console.log('Download completed.');
-
-    // if videos and audios are not equal in length, something went wrong
-    if (ytVideoUrls.length !== audios.length) {
-        throw new Error('Mismatch in video and audio downloads.');
-    }
-
-    console.log('Processing with FFmpeg...');
+async function downloadAndProcessVideos(playlistUrl) {
+    const playlist = await ytpl(playlistUrl);
+    const videoUrls = playlist.items.map(item => item.url);
 
     const processedVideos = [];
-    for (let i = 0; i < ytVideoUrls.length; i++) {
-        const videoPath = videos[i];
-        const audioPath = audios[i];
+    for (const videoUrl of videoUrls) {
+        const videoPath = await downloadVideo(videoUrl);
+        const audioPath = await downloadAudio(videoUrl);
         const processedVideo = await processWithFFmpeg(videoPath, audioPath);
         processedVideos.push(processedVideo);
     }
@@ -40,19 +18,5 @@ async function downloadAndProcessVideos(ytVideoUrls) {
     console.log('All done.');
 }
 
-let videoUrls = []
-
 const playlistUrl = 'https://www.youtube.com/playlist?list=PLRWvNQVqAeWIcz0Ky6pnrKkqsp3MA7o_n';
-videoUrls = getVideoUrlsFromPlaylist(playlistUrl)
-
-if (!playlistUrl && videoUrls.length === 0) {
-    // If not using a playlist, you can manually add video URLs
-    videoUrls = [
-        'https://www.youtube.com/watch?v=nm28m4gEOkI',
-        'https://www.youtube.com/watch?v=kx8yLdUJbqs',
-        'https://www.youtube.com/watch?v=xwU_MpVXpM4'
-    ];
-}
-
-console.log('Video URLs:', videoUrls);
-downloadAndProcessVideos(videoUrls).catch(console.error);
+downloadAndProcessVideos(playlistUrl).catch(console.error);
