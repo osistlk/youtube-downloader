4bb69a72762f2e40e16cfbdc329fc6e357f52575
osistlk <osistlk@gmail.com>
2024-05-16 20:35:59 -0400

    formatting

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-const { downloadVideo, downloadAudio, processWithFFmpeg } = require("./download_yt")
-const { fetchPlaylistShortURLs } = require("./read_playlist")
+const {
+  downloadVideo,
+  downloadAudio,
+  processWithFFmpeg,
+} = require("./download_yt");
+const { fetchPlaylistShortURLs } = require("./read_playlist");
 
 async function downloadAndProcessVideos(ytVideoUrls) {
-    let videoPromises = []
-    let audioPromises = []
+  let videoPromises = [];
+  let audioPromises = [];
 
-    console.log('Downloads starting...')
-    for (const videoUrl of ytVideoUrls) {
-        videoPromises.push(downloadVideo(videoUrl))
-        audioPromises.push(downloadAudio(videoUrl))
-    }
+  console.log("Downloads starting...");
+  for (const videoUrl of ytVideoUrls) {
+    videoPromises.push(downloadVideo(videoUrl));
+    audioPromises.push(downloadAudio(videoUrl));
+  }
 
-    const videos = await Promise.all(videoPromises)
-    const audios = await Promise.all(audioPromises)
-    console.log('Downloads completed.')
+  const videos = await Promise.all(videoPromises);
+  const audios = await Promise.all(audioPromises);
+  console.log("Downloads completed.");
 
-    // if videos and audios are not equal in length, something went wrong
-    if (ytVideoUrls.length !== audios.length) {
-        throw new Error('Mismatch in video and audio downloads.')
-    }
+  // if videos and audios are not equal in length, something went wrong
+  if (ytVideoUrls.length !== audios.length) {
+    throw new Error("Mismatch in video and audio downloads.");
+  }
 
-    console.log('Processing with FFmpeg...')
+  console.log("Processing with FFmpeg...");
 
-    const processedVideos = []
-    for (let i = 0; i < ytVideoUrls.length; i++) {
-        const videoPath = videos[i].path
-        const audioPath = audios[i].path
-        const videoTitle = videos[i].title
-        const processedVideo = await processWithFFmpeg(videoPath, audioPath, videoTitle)
-        processedVideos.push(processedVideo)
-    }
+  const processedVideos = [];
+  for (let i = 0; i < ytVideoUrls.length; i++) {
+    const videoPath = videos[i].path;
+    const audioPath = audios[i].path;
+    const videoTitle = videos[i].title;
+    const processedVideo = await processWithFFmpeg(
+      videoPath,
+      audioPath,
+      videoTitle,
+    );
+    processedVideos.push(processedVideo);
+  }
 
-    console.log('Processing completed.')
-    console.log('All done.')
+  console.log("Processing completed.");
+  console.log("All done.");
 }
 
 async function main() {
-    const playlistUrl = 'https://www.youtube.com/playlist?list=PLRWvNQVqAeWLPYrIW3bUWik62khdhk2Ro'
-    const videoUrls = await fetchPlaylistShortURLs(playlistUrl)
+  const playlistUrl =
+    "https://www.youtube.com/playlist?list=PLRWvNQVqAeWLPYrIW3bUWik62khdhk2Ro";
+  const videoUrls = await fetchPlaylistShortURLs(playlistUrl);
 
-    console.log('Video URLs:', videoUrls)
-    await downloadAndProcessVideos(videoUrls)
+  console.log("Video URLs:", videoUrls);
+  await downloadAndProcessVideos(videoUrls);
 }
 
-main()
+main();
--- read_playlist.js
+++ read_playlist.js
@@ -1,23 +1,23 @@
-const ytpl = require("ytpl")
+const ytpl = require("ytpl");
 
 /**
  * Fetches short URLs for each video in given YouTube playlist and logs them to the console.
  * @param {string} playlistUrl - The URL of the targeted YouTube playlist.
  */
 async function fetchPlaylistShortURLs(playlistUrl) {
-    try {
-        // Parse the specified playlist into an object structure with videos as properties
-        const parsedPlaylist = await ytpl(playlistUrl);
+  try {
+    // Parse the specified playlist into an object structure with videos as properties
+    const parsedPlaylist = await ytpl(playlistUrl);
 
-        // Extract only the shortened URLs, ignoring other metadata about individual videos
-        const urlsArray = parsedPlaylist.items.map(video => video.shortUrl);
+    // Extract only the shortened URLs, ignoring other metadata about individual videos
+    const urlsArray = parsedPlaylist.items.map((video) => video.shortUrl);
 
-        return urlsArray
-    } catch (error) {
-        // Handle any errors that may occur during parsing or processing
-        console.error(`An error occurred while fetching playlist data: ${error}`);
-        return []
-    }
+    return urlsArray;
+  } catch (error) {
+    // Handle any errors that may occur during parsing or processing
+    console.error(`An error occurred while fetching playlist data: ${error}`);
+    return [];
+  }
 }
 
-module.exports = { fetchPlaylistShortURLs }
+module.exports = { fetchPlaylistShortURLs };
