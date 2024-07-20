fa9f08e136343de494d04f5f365eff1a62761771
osistlk <osistlk@gmail.com>
2024-07-20 18:16:32 -0400

    chore: Fix formatting in download_yt.js and ffmpeg_pl.js

--- ffmpeg_pl.js
+++ ffmpeg_pl.js
@@ -53,14 +53,14 @@ ytpl(playlistUrl).then((playlist) => {
         const toMB = (i) => (i / 1024 / 1024).toFixed(2);
 
         process.stdout.write(
-          `Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `,
+          `Audio  | ${((tracker.audio.downloaded / tracker.audio.total) * 100).toFixed(2)}% processed `,
         );
         process.stdout.write(
           `(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${" ".repeat(10)}\n`,
         );
 
         process.stdout.write(
-          `Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `,
+          `Video  | ${((tracker.video.downloaded / tracker.video.total) * 100).toFixed(2)}% processed `,
         );
         process.stdout.write(
           `(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${" ".repeat(10)}\n`,
