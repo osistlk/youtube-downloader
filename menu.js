f8d731523b43c6f13819950dbd6584322b3cc186
osistlk <osistlk@gmail.com>
2024-09-08 20:22:24 -0400

    Full menu to download one video

--- menu.js
+++ menu.js
@@ -34,17 +34,56 @@ const ytdl = require("@distube/ytdl-core");
                     message: 'Video URL:'
                 });
                 const answer = await prompt.run();
+
                 const id = ytdl.getURLVideoID(answer);
                 const info = await ytdl.getInfo(id);
+
                 const formats = info.formats.filter(format => format.hasAudio || format.hasVideo);
                 const videoFormats = formats.filter(format => !format.hasAudio && format.hasVideo);
                 const audioFormats = formats.filter(format => format.hasAudio && !format.hasVideo);
-                const videoFormatPropt = new Select({
-                    name: 'video format',
-                    message: 'Select a video format',
-                    choices: videoFormats.map(format => format.qualityLabel)
+
+                const videoQualities = Array.from(new Set(videoFormats.map(format => format.qualityLabel))).sort((a, b) => {
+                    const na = Number(a.replace('p', ''));
+                    const nb = Number(b.replace('p', ''));
+                    return na > nb ? -1 : na < nb ? 1 : 0;
                 });
-                const videoFormatAnswer = await videoFormatPropt.run();
+                const videoContainers = Array.from(new Set(videoFormats.map(format => format.container))).sort();
+                const audioBitrates = Array.from(new Set(audioFormats.map(format => format.audioBitrate))).sort((a, b) => (a > b ? -1 : a < b ? 1 : 0));
+                const audioContainers = Array.from(new Set(audioFormats.map(format => format.container))).sort();
+
+                const videoQualityPrompt = new Select({
+                    name: 'video format',
+                    message: 'Select a video quality (higher is better/larger)',
+                    choices: videoQualities
+                });
+                const videoContainerPrompt = new Select({
+                    name: 'video container',
+                    message: 'Select a video container (I prefer mp4/h264)',
+                    choices: videoContainers
+                })
+                const audioBitratePrompt = new Select({
+                    name: 'audio bitrate',
+                    message: 'Select a audio bitrate (higher is better/larger)',
+                    choices: audioBitrates.map(bitrate => '' + bitrate)
+                });
+                const audioContainerPrompt = new Select({
+                    name: 'audio container',
+                    message: 'Select a audio container (I prefer mp4a/aac',
+                    choices: audioContainers
+                })
+
+                const videoQualityAnswer = await videoQualityPrompt.run();
+                const videoContainerAnswer = await videoContainerPrompt.run();
+                const audioBitrateAnswer = await audioBitratePrompt.run();
+                const audioContainerAnswer = await audioContainerPrompt.run();
+
+                const videoStream = ytdl(answer, { filter: format => format.qualityLabel === videoQualityAnswer && format.container === videoContainerAnswer });
+                const audioStream = ytdl(answer, { filter: format => format.audioBitrate === audioBitrateAnswer && format.container === audioContainerAnswer });
+
+                const videoOutput = path.join(TEMP_DIR, `${title}_video.mp4`);
+                const audioOutput = path.join(TEMP_DIR, `${title}_audio.mp4a`);
+                const finalOutput = path.join(OUTPUT_DIR, `${title}.mp4`);
+
                 break;
             case "playlist":
                 console.log(2);
