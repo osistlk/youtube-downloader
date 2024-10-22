1a86281260bd627a38fec5dc219e43c79398155e
jacquesdaytona <osistlk@gmail.com>
2024-10-21 20:41:02 -0400

    Refactor API endpoint to use objects instead of arrays for queue and history

--- src/api/index.js
+++ src/api/index.js
@@ -13,124 +13,124 @@ const history = {};
 const expired = [];
 
 router.get("/youtube/:id/formats", async (ctx) => {
-    const videoId = ctx.params.id;
-    const info = await ytdl.getInfo(videoId);
-    const audioFormats = info.formats.filter(
-        (format) => format.hasAudio && !format.hasVideo,
-    );
-    const videoFormats = info.formats.filter(
-        (format) => format.hasVideo && !format.hasAudio,
-    );
+  const videoId = ctx.params.id;
+  const info = await ytdl.getInfo(videoId);
+  const audioFormats = info.formats.filter(
+    (format) => format.hasAudio && !format.hasVideo,
+  );
+  const videoFormats = info.formats.filter(
+    (format) => format.hasVideo && !format.hasAudio,
+  );
 
-    const uniqueAudioFormats = Array.from(
-        new Set(audioFormats.map((format) => format.itag)),
-    )
-        .map((itag) => audioFormats.find((format) => format.itag === itag))
-        .sort((a, b) => b.audioBitrate - a.audioBitrate);
+  const uniqueAudioFormats = Array.from(
+    new Set(audioFormats.map((format) => format.itag)),
+  )
+    .map((itag) => audioFormats.find((format) => format.itag === itag))
+    .sort((a, b) => b.audioBitrate - a.audioBitrate);
 
-    const uniqueVideoFormats = Array.from(
-        new Set(videoFormats.map((format) => format.itag)),
-    )
-        .map((itag) => videoFormats.find((format) => format.itag === itag))
-        .sort((a, b) => {
-            const getWidth = (qualityLabel) =>
-                parseInt(qualityLabel.replace("p", ""), 10);
-            return getWidth(b.qualityLabel) - getWidth(a.qualityLabel);
-        });
+  const uniqueVideoFormats = Array.from(
+    new Set(videoFormats.map((format) => format.itag)),
+  )
+    .map((itag) => videoFormats.find((format) => format.itag === itag))
+    .sort((a, b) => {
+      const getWidth = (qualityLabel) =>
+        parseInt(qualityLabel.replace("p", ""), 10);
+      return getWidth(b.qualityLabel) - getWidth(a.qualityLabel);
+    });
 
-    ctx.body = {
-        audioFormats: uniqueAudioFormats,
-        videoFormats: uniqueVideoFormats,
-    };
+  ctx.body = {
+    audioFormats: uniqueAudioFormats,
+    videoFormats: uniqueVideoFormats,
+  };
 });
 
 router.get("/youtube/:id/download/:itag", async (ctx) => {
-    const videoId = ctx.params.id;
-    const itag = ctx.params.itag;
-    const basicInfo = await ytdl.getInfo(videoId);
-    const extension = basicInfo.formats.find(
-        (format) => format.itag == itag,
-    ).container;
-    const url = `https://www.youtube.com/watch?v=${videoId}`;
-    const stream = ytdl(url, { quality: itag });
-    ctx.set(
-        "Content-Disposition",
-        `attachment; filename="${videoId}.${itag}.${extension}"`,
-    );
-    ctx.body = stream;
+  const videoId = ctx.params.id;
+  const itag = ctx.params.itag;
+  const basicInfo = await ytdl.getInfo(videoId);
+  const extension = basicInfo.formats.find(
+    (format) => format.itag == itag,
+  ).container;
+  const url = `https://www.youtube.com/watch?v=${videoId}`;
+  const stream = ytdl(url, { quality: itag });
+  ctx.set(
+    "Content-Disposition",
+    `attachment; filename="${videoId}.${itag}.${extension}"`,
+  );
+  ctx.body = stream;
 });
 
 router.get("/youtube/:id/queue/:itag", async (ctx) => {
-    const videoId = ctx.params.id;
-    const itag = ctx.params.itag;
-    const id = uuidv4();
-    const timestamp = new Date().toISOString();
-    const retries = 3;
-    queue[id] = { videoId, itag, timestamp, retries };
-    eventEmitter.emit("queueAdded", { id, videoId, itag, timestamp, retries });
-    ctx.body = {
-        message: "Added to queue.",
-        id,
-        videoId,
-        itag,
-        timestamp,
-        retries,
-    };
+  const videoId = ctx.params.id;
+  const itag = ctx.params.itag;
+  const id = uuidv4();
+  const timestamp = new Date().toISOString();
+  const retries = 3;
+  queue[id] = { videoId, itag, timestamp, retries };
+  eventEmitter.emit("queueAdded", { id, videoId, itag, timestamp, retries });
+  ctx.body = {
+    message: "Added to queue.",
+    id,
+    videoId,
+    itag,
+    timestamp,
+    retries,
+  };
 });
 
 router.get("/queue", async (ctx) => {
-    ctx.body = queue;
+  ctx.body = queue;
 });
 
 router.get("/history", async (ctx) => {
-    ctx.body = history;
+  ctx.body = history;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => {
-    console.log("Server is running at http://localhost:3000");
+  console.log("Server is running at http://localhost:3000");
 });
 
 // Add event listeners
 eventEmitter.on("queueAdded", async (data) => {
-    console.log("New item added to queue:", data);
-    eventEmitter.emit("historyAdded", data);
-    // Download itag format to disk
-    const { videoId, itag } = data;
-    const basicInfo = await ytdl.getInfo(videoId);
-    const extension = basicInfo.formats.find(
-        (format) => format.itag == itag,
-    ).container;
-    const url = `https://www.youtube.com/watch?v=${videoId}`;
-    const stream = ytdl(url, { quality: itag });
-    const outputDir = "./downloads";
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir, { recursive: true });
-    }
-    const output = `${outputDir}/${videoId}.${itag}.${extension}`;
-    stream
-        .pipe(fs.createWriteStream(output))
-        .on("finish", () => {
-            // Remove job from queue
-            delete queue[data.id];
-            // Add to history
-            history[data.id] = { ...data, output };
-            console.log(`Download finished for ${videoId}.${itag}.${extension}`);
-        })
-        .on("error", (err) => {
-            console.error(`Error downloading ${videoId}.${itag}.${extension}:`, err);
-            if (queue[data.id].retries > 0) {
-                queue[data.id].retries -= 1;
-                console.log(
-                    `Retries left for ${videoId}.${itag}.${extension}: ${queue[data.id].retries}`,
-                );
-            } else {
-                console.log(
-                    `No retries left for ${videoId}.${itag}.${extension}. Removing from queue.`,
-                );
-                delete queue[data.id];
-                expired.push(data);
-            }
-        });
+  console.log("New item added to queue:", data);
+  eventEmitter.emit("historyAdded", data);
+  // Download itag format to disk
+  const { videoId, itag } = data;
+  const basicInfo = await ytdl.getInfo(videoId);
+  const extension = basicInfo.formats.find(
+    (format) => format.itag == itag,
+  ).container;
+  const url = `https://www.youtube.com/watch?v=${videoId}`;
+  const stream = ytdl(url, { quality: itag });
+  const outputDir = "./downloads";
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+  const output = `${outputDir}/${videoId}.${itag}.${extension}`;
+  stream
+    .pipe(fs.createWriteStream(output))
+    .on("finish", () => {
+      // Remove job from queue
+      delete queue[data.id];
+      // Add to history
+      history[data.id] = { ...data, output };
+      console.log(`Download finished for ${videoId}.${itag}.${extension}`);
+    })
+    .on("error", (err) => {
+      console.error(`Error downloading ${videoId}.${itag}.${extension}:`, err);
+      if (queue[data.id].retries > 0) {
+        queue[data.id].retries -= 1;
+        console.log(
+          `Retries left for ${videoId}.${itag}.${extension}: ${queue[data.id].retries}`,
+        );
+      } else {
+        console.log(
+          `No retries left for ${videoId}.${itag}.${extension}. Removing from queue.`,
+        );
+        delete queue[data.id];
+        expired.push(data);
+      }
+    });
 });
