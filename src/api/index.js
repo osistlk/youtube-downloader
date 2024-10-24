06167943334b7ba3ba937c1592ab94b86ad55f68
jacquesdaytona <osistlk@gmail.com>
2024-10-23 21:15:42 -0400

    Refactor API endpoint to remove unused code and simplify setup

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,14 @@
 const Koa = require("koa");
 const Router = require("@koa/router");
-const EventEmitter = require("events");
 
 const { setupRoutes } = require("./routes");
 const { setupEventListeners } = require("./events");
 
 const app = new Koa();
 const router = new Router();
-const eventEmitter = new EventEmitter();
 
-setupRoutes(router, eventEmitter);
-setupEventListeners(eventEmitter);
+setupRoutes(router);
+setupEventListeners();
 
 app.use(router.routes()).use(router.allowedMethods());
 
