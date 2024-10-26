b88a3d4b1fc4dee10e3b705510b03cd922a394a9
jacquesdaytona <osistlk@gmail.com>
2024-10-26 16:21:12 -0400

    Refactor code to improve UI readability and consistency

--- src/web/bundle.js
+++ src/web/bundle.js
@@ -11,74 +11,82 @@ parseFormatsResponse = (data) => {
   return { audioFormats, videoFormats };
 };
 
+const handleSuccess = (data) => {
+  let { audioFormats, videoFormats } = parseFormatsResponse(data);
+  audioFormats = audioFormats.filter(format => format.audioBitrate > 48);
+  videoFormats = videoFormats.filter(format => {
+    const quality = parseInt(format.qualityLabel.slice(0, -1));
+    return quality > 480;
+  });
+  $("#audioFormats").empty();
+  $("#videoFormats").empty();
+  // Create table headers
+  $("#audioFormats").append(`
+        <tr>
+        <th>Itag</th>
+        <th>Container</th>
+        <th>Audio Bitrate</th>
+        <th>Audio Sample Rate</th>
+        <th>Audio Codec</th>
+        <th>Size</th>
+        </tr>
+    `);
+  $("#videoFormats").append(`
+        <tr>
+        <th>Itag</th>
+        <th>Container</th>
+        <th>Bitrate</th>
+        <th>Video Codec</th>
+        <th>Quality</th>
+        <th>FPS</th>
+        <th>Size</th>
+        </tr>
+    `);
+
+  // Append audio formats to the table
+  audioFormats.forEach((format) => {
+    const contentLengthMB = isNaN(format.contentLength)
+      ? ""
+      : (format.contentLength / (1024 * 1024)).toFixed(2);
+    $("#audioFormats").append(`
+        <tr>
+        <td>${format.itag || ""}</td>
+        <td>${format.container || ""}</td>
+        <td>${format.audioBitrate || ""}</td>
+        <td>${format.audioSampleRate || ""}</td>
+        <td>${format.audioCodec || ""}</td>
+        <td>${contentLengthMB} MB</td>
+        </tr>
+        `);
+  });
+
+  // Append video formats to the table
+  videoFormats.forEach((format) => {
+    const contentLengthMB = isNaN(format.contentLength)
+      ? ""
+      : (format.contentLength / (1024 * 1024)).toFixed(2);
+    $("#videoFormats").append(`
+        <tr>
+        <td>${format.itag || ""}</td>
+        <td>${format.container || ""}</td>
+        <td>${format.bitrate || ""}</td>
+        <td>${format.videoCodec || ""}</td>
+        <td>${format.qualityLabel || ""}</td>
+        <td>${format.fps || ""}</td>
+        <td>${contentLengthMB} MB</td>
+        </tr>
+        `);
+  });
+};
+
 const listFormats = () => {
   const videoId = parseVideoId($("#url").val());
   $.ajax({
     url: `${url}/${videoId}/formats`,
     method: "GET",
-    success: (data) => {
-      const { audioFormats, videoFormats } = parseFormatsResponse(data);
-      $("#audioFormats").empty();
-      $("#videoFormats").empty();
-      // Create table headers
-      $("#audioFormats").append(`
-              <tr>
-              <th>Itag</th>
-              <th>Container</th>
-              <th>Audio Bitrate</th>
-              <th>Audio Sample Rate</th>
-              <th>Audio Codec</th>
-              <th>Size</th>
-              </tr>
-          `);
-      $("#videoFormats").append(`
-              <tr>
-              <th>Itag</th>
-              <th>Container</th>
-              <th>Bitrate</th>
-              <th>Video Codec</th>
-              <th>Quality</th>
-              <th>FPS</th>
-              <th>Size</th>
-              </tr>
-          `);
-
-      // Append audio formats to the table
-      audioFormats.forEach((format) => {
-        const contentLengthMB = isNaN(format.contentLength)
-          ? ""
-          : (format.contentLength / (1024 * 1024)).toFixed(2);
-        $("#audioFormats").append(`
-              <tr>
-              <td>${format.itag || ""}</td>
-              <td>${format.container || ""}</td>
-              <td>${format.audioBitrate || ""}</td>
-              <td>${format.audioSampleRate || ""}</td>
-              <td>${format.audioCodec || ""}</td>
-              <td>${contentLengthMB} MB</td>
-              </tr>
-              `);
-      });
-
-      // Append video formats to the table
-      videoFormats.forEach((format) => {
-        const contentLengthMB = isNaN(format.contentLength)
-          ? ""
-          : (format.contentLength / (1024 * 1024)).toFixed(2);
-        $("#videoFormats").append(`
-              <tr>
-              <td>${format.itag || ""}</td>
-              <td>${format.container || ""}</td>
-              <td>${format.bitrate || ""}</td>
-              <td>${format.videoCodec || ""}</td>
-              <td>${format.qualityLabel || ""}</td>
-              <td>${format.fps || ""}</td>
-              <td>${contentLengthMB} MB</td>
-              </tr>
-              `);
-      });
-    }
+    success: handleSuccess
   });
+
 };
 
 $(document).ready(() => {
