fb1c4c3b9715da99dacf2bd2c69e87df591b27d1
jacquesdaytona <osistlk@gmail.com>
2024-11-17 12:44:24 -0500

    Add caption downloading functionality to CLI script

--- src/cli/get-caption.js
+++ src/cli/get-caption.js
@@ -22,4 +22,28 @@
     // download video based on itag
     const id = ytdl.getURLVideoID(url);
     const info = await ytdl.getInfo(id);
+    const caption = info.player_response.captions.playerCaptionsTracklistRenderer.captionTracks.find((caption) => caption.languageCode == languageCode);
+    const title = info.videoDetails.title;
+    const sanitizedTitle = sanitize(title);
+    const filename = `${sanitizedTitle}.caption${caption.vssId}`;
+    const output = `./${filename}`;
+
+    console.log(`Downloading ${title}...`);
+    const stream = ytdl.downloadFromInfo(info, { lang: languageCode });
+
+    let downloaded = 0;
+    const total = caption.contentLength;
+
+    stream.on("progress", (chunkLength, downloadedBytes, totalBytes) => {
+        downloaded += chunkLength;
+        const percent = ((downloaded / total) * 100).toFixed(2);
+        process.stdout.write(`Downloading: ${percent}%\r`);
+    });
+    stream.pipe(fs.createWriteStream(output));
+
+    await new Promise((resolve, reject) => {
+        stream.on("finish", resolve);
+        stream.on("error", reject);
+    });
+    console.log(`Downloaded to ${output}`);
 })();
