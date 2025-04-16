7d282a21adb29750ebf978761d8dbdea2254225d
osistlk <osistlk@gmail.com>
2025-04-15 20:18:25 -0400

    Add sleep command to test-pending script for improved timing between requests

--- test-pending.sh
+++ test-pending.sh
@@ -1,4 +1,5 @@
 curl -X GET "http://localhost:3000/youtube/WEb-TmacK-c/info/formats?type=video&height=1080" | python3 -m json.tool
+sleep 5
 for i in {1..1}; do
     curl -X POST "http://localhost:3000/youtube/pending" -H "Content-Type: application/json" -d '{"videoId": "WEb-TmacK-c", "itag": 137}' | python3 -m json.tool
 done
